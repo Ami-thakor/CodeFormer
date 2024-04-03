236f37380cb05e658b09a5329c3119ed45bd86ef
Ami-thakor <128568711+Ami-thakor@users.noreply.github.com>
2024-04-03 10:28:46 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,15 +1,15 @@
 import os
 
 # Clone the CodeFormer repository
-os.system("git clone https://github.com/sczhou/CodeFormer")
-os.chdir("CodeFormer")
+# os.system("git clone https://github.com/sczhou/CodeFormer")
+# os.chdir("CodeFormer")
 
 # Create a new Anaconda environment
 os.system("conda create -n codeformer python=3.8 -y")
 os.system("conda activate codeformer")
 
 # Install Python dependencies
-os.system("pip3 install -r requirements.txt")
+# os.system("pip3 install -r requirements.txt")
 os.system("python basicsr/setup.py develop")
 
 # Install dlib (Optional)
